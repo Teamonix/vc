ffc163f293cee93a32d19ee2b5dcbab3822a08b7
Teamonix <unioxo@outlook.com>
2024-12-06 20:00:04 +0530

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -1,1 +1,23 @@
+from pyrogram import Client, filters
+from config import *
+from pytgcalls import PyTgCalls
 
+
+
+bot = Client(
+    name="Bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="YukkiMusic.plugins"),
+)
+
+app = Client(
+    name="Assistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=str(STRING_SESSION),
+)
+
+
+call = PyTgCalls(app)
